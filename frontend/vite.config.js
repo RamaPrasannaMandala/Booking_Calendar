22700c334a389203071bb4e5545822565a9748cf
RamaPrasannaMandala <ramaprasanna2709@gmail.com>
2025-09-07 14:05:02 +0530

    Frontend ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // <-- important for correct relative paths in production
   server: {
     port: 5173,
     open: true,
